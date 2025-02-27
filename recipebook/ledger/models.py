e3f176ca4a62e23e52c60a9d04efcefc82ae4a4e
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-02-27 15:49:07 +0800

    Updated models.py
    3 new models were added

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger_detail', args=[str(self.name)])
+        
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:detail', args=[str(self.name)])
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    quantity = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.quantity + " " + self.ingredient.name
+    
