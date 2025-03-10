4a5f81a5ba22c6c9834cf87d71ce23161ef5e94a
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-10 16:40:09 +0800

    Added an admin panel and began to implement a profile model into models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(validators=[ MinLengthValidator(255, 'the field must contain at least 50 characters') ])
 
+    def __str__(self):
+        return self.name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
