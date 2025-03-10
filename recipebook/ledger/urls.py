c533c3af54606ac8c8dd7ef98cbb6057d2d0c165
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-10 17:37:28 +0800

    Changed files to match the specs

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail
+from .views import recipe_list, recipe_detail, custom_login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('recipes/', recipe_list, name='list'),
     path('recipe/<str:recipe_name>/', recipe_detail, name='detail'),
+    path('login/', custom_login, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 from .models import Recipe
 
@@ -15,3 +17,14 @@ def recipe_detail(request, recipe_name):
             recipe_list = recipe
     return render(request, "task_list.html", {"recipe": recipe})    
 
+def custom_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("ledger:list")  
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
