3c6bc745c9ed1dbccbf93a7e254090970e1a7af7
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-10 18:21:30 +0800

    Bug fixing and created a base.html

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,20 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
+from .models import Recipe, RecipeIngredient
 
-from .models import Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, "task_lists.html", {"recipes": recipes})
 
 def recipe_detail(request, recipe_name):
-    recipes = Recipe.objects.filter(name=recipe_name).first()
-    recipe_list = None
-    for recipe in recipes:
-        if recipe.name == recipe_name:
-            recipe_list = recipe
-    return render(request, "task_list.html", {"recipe": recipe})    
+    recipe = Recipe.objects.filter(name=recipe_name).first()
+    ingredients = RecipeIngredient.objects.filter(recipe=recipe)
+    return render(request, "task_list.html", {"recipe": recipe, "ingredients": ingredients})
 
 def custom_login(request):
     if request.method == "POST":
