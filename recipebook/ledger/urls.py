8f3166d8592d5586526dcf091388b73d0ee68511
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-24 16:18:57 +0800

    made changes to url.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
 ]
 
 app_name = "ledger"
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
